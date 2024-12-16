14b86bf5138f736d52fbdfe1ada9a503762f8f4e
Bukevsky <bykaplay@gmail.com>
2024-12-16 16:58:59 +0300

    update backend url

--- tu152-main/src/services/api.js
+++ tu152-main/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api/v1', // Базовый URL, на основе вашего бэкенда
+    baseURL: 'http://localhost:8081/api/v1', // Базовый URL, на основе вашего бэкенда
 });
 
 export default {
--- tu152-main/vue.config.js
+++ tu152-main/vue.config.js
@@ -1,11 +1,2 @@
 module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // Адрес вашего бэкенда
-        changeOrigin: true, // Для корректной работы CORS
-        pathRewrite: { '^/api': '' }, // Убирает /api из пути при отправке на бэкенд
-      },
-    },
-  },
 };
