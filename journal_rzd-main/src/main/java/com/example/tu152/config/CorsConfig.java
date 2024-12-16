3b96d17c55f594c0cf60730d7c1e6f6642890512
Bukevsky <bykaplay@gmail.com>
2024-12-16 19:26:01 +0300

    update backend url

--- journal_rzd-main/src/main/java/com/example/tu152/config/CorsConfig.java
+++ journal_rzd-main/src/main/java/com/example/tu152/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.tu152.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,17 +12,31 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
+    @Value("spring.rest.frontend.url")
+    private String route;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:8080")); // Адрес фронтенда
+        config.setAllowedOrigins(Arrays.asList(route));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
+    // Как spring security добавлю, переделать под реализацию снизу
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/api/v1/**").allowedOrigins(route);
+//            }
+//        };
+//    }
 }
